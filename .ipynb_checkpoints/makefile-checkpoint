3c0a64ad875b7af708a21ff54c5de62b0349b7e7
nedwardsthro <nedwardsthro@gmail.com>
2023-09-29 02:54:56 +0000

    linting

--- .ipynb_checkpoints/makefile-checkpoint
+++ .ipynb_checkpoints/makefile-checkpoint
@@ -5,7 +5,11 @@ env:
 	source env/bin/activate
 
 run:
+	source env/bin/activate
 	@python bin/clockdeco_param.py
 .PHONY: tests
 tests:
 	pytest -vv tests
+
+lint:
+	pylint bin/perceptron.py
---
+++ bin/perceptron.py
@@ -0,0 +1,33 @@
+'''
+This is my docstring. This class is used to be in a test to see if the linting works. 
+'''
+class Perceptron():
+    '''
+    This is my docstring. This class is used to be in a test to see if the linting works. 
+    '''
+    def __init__(self, weights):
+        self._weights = weights
+    def train(self, inputs, labels):
+        '''
+        This module trains the model. It requires two inputs to the model:
+            inputs: independent variable to train the model
+            labels: dependent variable that is the outcome of the model
+        '''
+        dummied_inputs = [xer + [-1] for xer in inputs]
+        self._weights = [0.2] * len(dummied_inputs[0])
+        for _ in range(5000):
+            for train_input, label in zip(dummied_inputs, labels):
+                label_delta = label - self.predict(train_input)
+                for index, xer in enumerate(train_input):
+                    self._weights[index] += .1 * xer * label_delta
+    def predict(self, predict_input):
+        '''
+        This model predicts using the model created with train.
+        The only input is the data used to predict the label.
+        '''
+        if len(predict_input) == 0:
+            return None
+        predict_input = predict_input + [-1]
+        #pylint: disable = R1728
+        return int(0 < sum([x[0]*x[1] for x in zip(self._weights, predict_input)]))
+        
